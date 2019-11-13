127591cfc252551f16df452c379259381d5a6195
Nick Hudgins <nickhudgins@gmail.com>
2019-11-13 12:05:33 -0500

    landing page push from NH

--- 13-Capstone/Capstone.Web/DAL/ParkDao.cs
+++ 13-Capstone/Capstone.Web/DAL/ParkDao.cs
@@ -1,6 +1,7 @@
 ﻿using Capstone.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,6 +17,24 @@ namespace Capstone.Web.DAL
         public List<Park> GetAllParks()
         {
             List<Park> parks = new List<Park>();
+
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT parkName, state, parkDescription FROM park", conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Park park = new Park();
+
+                 park.ParkName= Convert.ToString(reader["parkName"]);
+                park.State = Convert.ToString(reader["state"]);
+                park.Description = Convert.ToString(reader["parkDescription"]);
+
+                parks.Add(park);
+            }
             return parks;
         }
         public Park GetPark(string parkCode)
