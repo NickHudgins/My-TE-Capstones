3c9f38569e404ef5b53ce886d66bbdf98e6f5c5d
Dean Gorsuch <dean.gorsuch@gmail.com>
2019-11-13 12:06:29 -0500

    Survey

--- 13-Capstone/Capstone.Web/Controllers/SurveyController.cs
+++ 13-Capstone/Capstone.Web/Controllers/SurveyController.cs
@@ -2,15 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone.Web.DAL;
+using Capstone.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Capstone.Web.Controllers
 {
     public class SurveyController : Controller
     {
+        ISurveyDao surveyDao;
+        IParkDao parkDao;
+
+        public SurveyController(ISurveyDao surveyDao, IParkDao parkDao)
+        {
+            this.surveyDao = surveyDao;
+            this.parkDao = parkDao;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Park> parks = parkDao.GetAllParks();
+
+            return View(parks);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Survey survey)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(survey);
+            }
+
+
+
+            return RedirectToAction("FavoritePark");
         }
         public IActionResult FavoritePark()
         {
--- 13-Capstone/Capstone.Web/DAL/SurveyDao.cs
+++ 13-Capstone/Capstone.Web/DAL/SurveyDao.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,38 @@ namespace Capstone.Web.DAL
     public class SurveyDao : ISurveyDao
     {
         private readonly string connectionString;
+        private readonly string sql_saveSurvey = "INSERT INTO survey_result (parkCode, emailAddress, state, activityLevel) VALUES" +
+            "(@parkCode, @emailAddress, @state, @activityLevel)";
+
         public SurveyDao(string connectionString)
         {
             this.connectionString = connectionString;
+        }
+
+        public Survey SaveSurvey(Survey survey)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = conn.CreateCommand();
+
+                    cmd.CommandText = sql_saveSurvey;
+                    cmd.Parameters.AddWithValue("@parkCode", survey.ParkCode);
+                    cmd.Parameters.AddWithValue("@emailAddress", survey.EmailAddress);
+                    cmd.Parameters.AddWithValue("@state", survey.State);
+                    cmd.Parameters.AddWithValue("@activityLevel", survey.ActivityLevel);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+                return survey;
         }
     }
 }
