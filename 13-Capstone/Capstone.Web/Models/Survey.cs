262f66299646f7bd7c8f41169d504d17c4a26f16
Dean Gorsuch <dean.gorsuch@gmail.com>
2019-11-17 13:19:10 -0500

    added alt text and custom error messages

--- 13-Capstone/Capstone.Web/Models/Survey.cs
+++ 13-Capstone/Capstone.Web/Models/Survey.cs
@@ -10,17 +10,17 @@ namespace Capstone.Web.Models
     {
         public int SurveyId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please select your favorite park.")]
         public string ParkCode { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage ="An email address is required.")]
+        [EmailAddress(ErrorMessage ="Please enter a valid email address")]
         public string EmailAddress { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please select your state of residence.")]
         public string State { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please select your activity level.")]
         public string ActivityLevel { get; set; }
 
         public List<Park> ParkList { get; set; }
